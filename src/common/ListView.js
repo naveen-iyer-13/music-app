b9f9d86e71e18710d3972dc3ab1928aa8758938e
VIVEK SRIVASTAVA <vivek@VIVEKs-MacBook-Air.local>
2018-05-12 14:34:20 +0530

    list view created

---
+++ src/common/ListView.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {
+  View,
+  StyleSheet,
+} from 'react-native'
+
+export const ListView = (props) => (
+  <View>
+    <View>
+    </View>
+    <View></View>
+    <View></View>
+  </View>
+)
+
+const styles = StyleSheet.create({})
