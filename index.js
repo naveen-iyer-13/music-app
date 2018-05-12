1d629af2feb948b8197380d2ffe22d1287406e49
Vivek Srivastava <vivsri95@gmail.com>
2018-05-13 01:11:50 +0530

    trending page api change

--- index.js
+++ index.js
@@ -10,11 +10,26 @@ import Trending from './src/screens/trending'
 import Library from './src/screens/library'
 import Footer from './src/common/Footer'
 
-const bibimpop = StackNavigator ({
+const StackNavigation = StackNavigator ({
   Trending: { screen: Trending},
   Library: { screen: Library},
 },{
   navigationOptions: {title: 'Welcome', header: null}
 });
+
+class bibimpop extends Component{
+  componentWillUnmount(){
+    this.clearStorage()
+  }
+
+  clearStorage = async() => {
+    AsyncStorage.clear(() => console.log('clearing local storage'))
+  }
+  render() {
+    return(
+      <StackNavigation />
+    )
+  }
+}
 
 AppRegistry.registerComponent('bibimpop', () => bibimpop);
---
+++ src/common/Modal.js
@@ -0,0 +1,29 @@
+// import React, { Component } from 'react'
+// import {
+//   StyleSheet,
+//   View,
+//   Text
+// } from 'react-native'
+//
+// export default const Modal = (Children) => {
+//   class Modal extends Component{
+//     contructor(super){
+//       super(props)
+//       this.state = {}
+//     }
+//
+//     render() {
+//       return(
+//         <View styles={styles.container}>
+//           <Children />
+//         </View>
+//       )
+//     }
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container:{
+//
+//   }
+// })
