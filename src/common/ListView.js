cb10c5131f23c164856e9ba7f0d8ad4645712a76
VIVEK SRIVASTAVA <vivek@VIVEKs-MacBook-Air.local>
2018-05-12 22:39:12 +0530

    library page done

--- src/common/ListView.js
+++ src/common/ListView.js
@@ -2,15 +2,46 @@ import React from 'react'
 import {
   View,
   StyleSheet,
+  Image,
+  Text
 } from 'react-native'
 
-export const ListView = (props) => (
-  <View>
-    <View>
+export const ListView = ({thumbnail, title}) => (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image style={styles.thumbnail} source={{uri: thumbnail}}/>
+      </View>
+      <View style={styles.songInfo}>
+        <Text>
+          {title}
+        </Text>
+      </View>
+      <View style={styles.actionBtn}></View>
     </View>
-    <View></View>
-    <View></View>
-  </View>
 )
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: 5,
+    marginBottom: 5
+  },
+  imageContainer:{
+    width: 50,
+    height: 50
+  },
+  thumbnail:{
+    width: '100%',
+    height: '100%'
+  },
+  actionBtn:{
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  songInfo: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
