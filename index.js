4e773eb0abcdb9db13e342c4241a13e4804a1193
Vivek Srivastava <vivsri95@gmail.com>
2018-05-12 22:39:12 +0530

    library page done

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ import {
   View
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-import Trending from './src/screens/trending/Trending.js'
+import Trending from './src/screens/trending'
+import Library from './src/screens/library'
+import Footer from './src/common/Footer'
 
 const bibimpop = StackNavigator ({
+  Library: { screen: Library},
   Trending: { screen: Trending},
-},{navigationOptions: {title: 'Welcome', header: null}});
+},{
+  navigationOptions: {title: 'Welcome', header: null}
+});
 
 AppRegistry.registerComponent('bibimpop', () => bibimpop);
---
+++ src/screens/library/Library.js
@@ -0,0 +1,109 @@
+
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  ScrollView
+} from 'react-native'
+import { Search } from './../../common/Search'
+import Footer from './../../common/Footer'
+import Songs from './components/Songs'
+import { searchSong } from './../../common/helpers'
+import {Trending} from './../../utils/assets/icons/top-100'
+
+let { height, width } = Dimensions.get('window')
+
+class Library extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      searchTerm: '',
+      tab: 'songs',
+      searchList: [],
+    }
+  }
+
+  handleSearch = (text) => {
+    searchSong(text, res => {
+      if(res)
+        this.setState({searchList: res})
+      else
+        this.setState({searchList: []})
+    })
+  }
+
+  handleTabPress = (tab) => {
+    this.setState({tab})
+  }
+
+  render() {
+    const { searchTerm, tab, searchList } = this.state
+    return (
+      <View style={styles.container}>
+        <View style={styles.screenContainer}>
+          <View style={styles.containerItem}>
+            <View style={styles.headerItem}>
+              <TouchableOpacity onPress={() => this.handleTabPress('songs')}>
+                <Text>Songs</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.headerItem}>
+              <TouchableOpacity onPress={() => this.handleTabPress('playlists')}>
+                <Text>Playlists</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <Search
+            searchTerm={searchTerm}
+            handleSearch={this.handleSearch}
+          />
+          <ScrollView>
+            {
+              tab === 'songs'
+              ?
+                <Songs searchList={searchList}/>
+              :
+              <View>
+                <Text>playlists</Text>
+                <Trending/>
+              </View>
+            }
+          </ScrollView>
+        </View>
+        <Footer/>
+      </View>
+    )
+  }
+}
+
+export default Library
+
+const styles = StyleSheet.create({
+  container:{
+    display: 'flex',
+    backgroundColor: '#FFFFFF',
+    width,
+    height
+  },
+  screenContainer:{
+    height: height - 80,
+    paddingRight: 12,
+    paddingLeft: 12
+  },
+  containerItem:{
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    height: '10%'
+  },
+  headerItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
