a1016c81ff653250595e32bbdaf8664b1a349380
VIVEK SRIVASTAVA <vivek@VIVEKs-MacBook-Air.local>
2018-05-13 15:07:52 +0530

    search artist added

--- src/screens/library/components/Songs.js
+++ src/screens/library/components/Songs.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react'
 import {
   View,
   Text,
-  AsyncStorage
+  AsyncStorage,
+  ScrollView
 } from 'react-native'
-import { getTrending } from './../../../common/helpers'
+// import { getTrending } from './../../../common/helpers'
 import { ListView } from './../../../common/ListView'
+import PopupModal from './../../../common/PopupModal'
+import { Search } from './../../../common/Search'
+
 
 class Songs extends Component{
   constructor(props){
     super(props)
     this.state = {
       list:[],
-      searchList: []
+      searchList: [],
+      popupModal: false,
+      searchTerm: '',
     }
   }
 
@@ -20,21 +26,26 @@ class Songs extends Component{
     this.getSongs()
   }
 
-  componentWillReceiveProps(nextProps){
-    if(nextProps.searchList){
-      this.setState({searchList: nextProps.searchList})
-    }
+  openModal = (song) => {
+    this.setState({popupModal: true, selectedSong: song})
+  }
+
+  closeModal = () => {
+    this.setState({popupModal: false})
   }
 
   getSongs(){
-    AsyncStorage.getItem('trendingSongs', (err, res) => {
+    AsyncStorage.getItem('library', (err, res) => {
       if(res)
         this.setState({list: JSON.parse(res), loading: false})
-      else
-        getTrending((list) => {
-          this.setState({list, loading: false})
-        })
     })
+  }
+
+  handleSearch = (text) => {
+    let { list } = this.state
+    this.setState({searchTerm: text})
+    let searchList = list.filter(song => song.title.toLowerCase().includes(text.toLowerCase()))
+    this.setState({searchList})
   }
 
   handleActions = (song) => {
@@ -53,16 +64,30 @@ class Songs extends Component{
   }
 
   render() {
-    let { list, searchList } = this.state
+    let { list, searchList, popupModal, selectedSong, searchTerm } = this.state
     // console.log(searchList);
-    list = searchList.length > 0 ? searchList : list
+    list = searchTerm? searchList : list
     return(
       <View>
+        <Search
+          searchTerm={searchTerm}
+          handleSearch={this.handleSearch}
+        />
+      <ScrollView>
         {
           list && list.map((song,index) => (
-            <ListView key={song.title + index} thumbnail={song.thumbnail} title={song.title} song={song} handleActions={this.handleActions}/>
+            <ListView
+              key={song.title + index}
+              thumbnail={song.thumbnail}
+              title={song.title}
+              song={song}
+              openModal={this.openModal}
+            />
           ))
         }
+      </ScrollView>
+
+        <PopupModal active={popupModal} closeModal={this.closeModal} song={selectedSong}/>
       </View>
     )
   }
